7f19575cb91ac10f185014242bc34fafc96f781f
Gavbriel <av.gabriel015@gmail.com>
2023-10-01 16:25:07 -0400

    FEM - Vista mobile terminada

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+
+      },
+      colors: {
+        naranja: '#FB923C',
+        cyan: '#016972',
+        verde: '#00403F'
+      }
+    },
+  },
+  plugins: [],
+}
+
